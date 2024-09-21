ed1758ff517128f01a5e06252087a18d5bff1f9d
aj3616 <123700125+aj3616@users.noreply.github.com>
2024-09-21 14:32:19 -0400

    simulate data
    right folder in scripts

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,14 +13,18 @@ library(tidyverse)
 
 
 #### Simulate data ####
-set.seed(304)
+set.seed(1007676409)
 
 # Define the start and end date
-start_date <- as.Date("2018-01-01")
-end_date <- as.Date("2023-12-31")
+start_date <- as.Date("2014-01-01")
+end_date <- as.Date("2024-08-21")
 
 # Set the number of random dates you want to generate
 number_of_dates <- 100
+
+data_ppm = read_csv("data/raw_data/raw_data.csv")
+leadaAmountPpm = as.numeric(data_ppm$`Lead Amount (ppm)`)
+mean_lead <- mean(leadaAmountPpm, na.rm = TRUE)
 
 data <-
   tibble(
@@ -30,11 +34,11 @@ data <-
         min = as.numeric(start_date),
         max = as.numeric(end_date)
       ),
-      origin = "1970-01-01"
+      origin = "2014-01-01"
     ),
-    number_of_marriage = rpois(n = number_of_dates, lambda = 15)
+    leadAmountppm = rgamma(n = number_of_dates, shape = 2, rate = 1 / mean_lead)
+    #leadAmountppm = rpois(n = number_of_dates, lambda = mean_lead)
   )
-
 
 #### Write_csv
 write_csv(data, file = "data/raw_data/simulated.csv")
